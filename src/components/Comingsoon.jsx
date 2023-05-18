1ceaa75345d3eab868d8ea93959d7b00a7b3e3d3
Nuwan Thilakarathna <44118134+iamnwn@users.noreply.github.com>
2023-05-18 10:43:02 +0530

    changed to coming soon

---
+++ src/components/Comingsoon.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ComingSoon.css";
+
+const Comingsoon = () => {
+  return (
+    <div>
+      <h2>Coming Soon . . . . .</h2>
+    </div>
+  );
+};
+
+export default Comingsoon;
